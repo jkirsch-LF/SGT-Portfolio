b065f41449234f23767c7af95206816296f0af2b
James Kirsch <jameskirschaf@gmail.com>
2018-02-08 16:08:10 -0800

    Finished Authentication and Security Features

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require('path');
+const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
-const session = require('express-session');
 const flash = require('connect-flash');
-const mongoose = require('mongoose');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 const PORT = 3000;
 
 // Mongoose Middleware
 mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/vidjot-dev')
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
-
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
 
 // HandleBars Middleware
 app.engine('handlebars', exphbs({
@@ -32,15 +36,22 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method-Override Middleware
 app.use(methodOverride('_method'));
 
-// Session-Express MiddleWare
+// Session-Express Middleware
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
+
+// Postport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect-Flash Middleware
 app.use(flash());
@@ -49,7 +60,8 @@ app.use(flash());
 app.use(function (req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
-    res.locals.err = req.flash('error');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 })
 
@@ -61,98 +73,14 @@ app.get('/', (req, res) => {
     });
 });
 
-
 // About Route
 app.get('/about', (req, res) => {
     res.render('about');
 })
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({ date: 'desc' })
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
-            });
-
-        })
-})
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-})
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            });
-        });
-});
-
-// Process Form
-app.post('/ideas', (req, res) => {
-    let errors = [];
-
-    if (!req.body.title) {
-        errors.push({ text: 'Please add a title' })
-    };
-
-    if (!req.body.details) {
-        errors.push({ text: "Please add some details" })
-    };
-
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Video idea added');
-                res.redirect('/ideas')
-            });
-    }
-});
-
-// Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            idea.title = req.body.title;
-            idea.details = req.body.details;
-
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', 'Video idea updated');
-                    res.redirect('/ideas');
-                });
-        });
-})
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-    Idea.remove({ _id: req.params.id })
-        .then(() => {
-            req.flash('success_msg', 'Video idea removed');
-            res.redirect('/ideas');
-        });
-});
+// Use Routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 //Listener
 app.listen(PORT, () => {
