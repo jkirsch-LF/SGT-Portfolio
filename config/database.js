3aab43705b9f6b8f349e606c2479cddd5fa10185
James Kirsch <jameskirschaf@gmail.com>
2018-02-08 20:12:45 -0800

    update

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://xilionk:Seifer184.@ds229878.mlab.com:29878/vidjot-prod'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
-}
