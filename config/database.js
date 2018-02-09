4a1a46c55603b964f4bcbc7a5e1684642a2b01d3
James Kirsch <jameskirschaf@gmail.com>
2018-02-08 16:19:16 -0800

    added production port config

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+  module.exports = {mongoURI: 'mongodb://<xilionk>:<Seifer184.>@ds229878.mlab.com:29878/vidjot-prod'}
+} else {
+  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+}
